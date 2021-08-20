21ecffb7722b7fdbaa4f428d42313ac2afe65024
Yash <yashp241195@gmail.com>
2021-08-20 09:58:22 +0530

    Update exercise_1_2.php

--- exercise_1_2.php
+++ exercise_1_2.php
@@ -1,6 +1,6 @@
 <?php
 // Exercise 1
-/*
+
 function isBitten()
 {
     $r_val = rand(0,1);
@@ -14,14 +14,14 @@ echo(isBitten());
 function part2(){
 echo((isBitten() == "TRUE")?"Charlie bit your finger!":"Charlie did not bite your finger!");
 }
-*/
+
 //part1();
 //part2();
 
 
 // Exercise 2
 
-/*
+
 function countWords($string){
 
 $token = strtok($string, " ");
@@ -29,30 +29,37 @@ $array = array();
 
 while ($token !== false)
 {
-   echo "$token<br>";
+   echo "$token ";
    $tck = strtolower($token);
    $array["$tck"] += 1;
    $token = strtok(" ");
 }
 
-print_r($array);
+//print_r($array);
+
+return $array;
 
 }
 
-function part1(){
+function part3(){
 
 $string = "Hello world Beautiful day today World";
 countWords($string);
 
 }
 
-*/
-
-// part1();
+//part3();
 
 ?>
 
-<html>  
+<html>
+
+<style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+</style>  
 <body>
 
 <form method="GET" action="<?php echo $_SERVER[‘PHP_SELF’]; ?>" >
@@ -63,11 +70,43 @@ countWords($string);
 
 <?php 
 
-// part 2
+// part 4
 
-echo $_GET["name"]; 
+function part4(){
+  echo $_GET["name"]; 
+}
+
+//part4();
+
+
+function part5(){
+
+$string = countWords($_GET["name"]);
+
+arsort($string);
+
+echo "<br/><br/>";
+
+echo "<table style='border 1px solid;'><tr><th>Word</th><th>Count</th></tr>";
+
+foreach($string as $s => $s_value) {
+  echo "<tr>";
+  echo "<td>" . $s . "</td> <td>" . $s_value . "</td>";
+  echo "</tr>";
+}
+
+
+echo " </table> ";
+
+
+}
+
+//$string = "Hello world Beautiful day today World";
+part5();
 
 ?>
+
+
 
 </body>
 </html>
